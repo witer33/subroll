872d684d977d85ff26b9ff1959f8f309d7460b24
witer33 <34513257+witer33@users.noreply.github.com>
2022-04-23 14:17:01 +0200

    moved tests

--- test.py
+++
@@ -1,9 +0,0 @@
-from src.subroll import SubRoll
-import requests
-
-roll = SubRoll("2a06:a003:5021::/48")
-
-for _ in range(100):
-    session = roll.get_session()
-    response = session.get("https://ipv6.wtfismyip.com/text")
-    print(response.text)
